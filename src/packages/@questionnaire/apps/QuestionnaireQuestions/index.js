2321f3cbb2aa7bbba2fce1a5213b0b5658f5803d
ts.mihail <mihail.salari@thinslices.com>
2020-04-15 12:22:32 +0300

    PHX-538 extract container max width into constant

--- src/packages/@questionnaire/apps/QuestionnaireQuestions/index.js
+++ src/packages/@questionnaire/apps/QuestionnaireQuestions/index.js
@@ -3,10 +3,14 @@ import styled from 'styled-components'
 import { Flex, Space } from '@kogaio'
 
 const HEADER_HEIGHT_MD = 60
+const QUESTIONNAIRE_CONTAINER_MAX_WIDTH = 950
 
 const QuestionnaireQuestions = () => (
   <Space mx={{ xs: 2, md: -4 }} mt='1px'>
-    <Wrapper justifyContent='center' bg={{ xs: 'white', md: 'questionnaireBg' }} height={{ md: `calc(100vh - ${HEADER_HEIGHT_MD}px)` }}>
+    <Wrapper
+      justifyContent='center'
+      bg={{ xs: 'white', md: 'questionnaireBg' }}
+      height={{ md: `calc(100vh - ${HEADER_HEIGHT_MD}px)` }}>
       <Space mx={{ md: 4 }} mt={{ md: 10 }} pb={{ md: 10 }} pt={{ md: 14 }} px={{ md: 24 }}>
         <Container
           bg='white'
@@ -17,7 +21,7 @@ const QuestionnaireQuestions = () => (
           flexDirection='column'
           height='fit-content'
           width={1}
-          maxWidth={{md: '950px'}}>
+          maxWidth={{ md: `${QUESTIONNAIRE_CONTAINER_MAX_WIDTH}px` }}>
           Content
         </Container>
       </Space>
