d0cc2e9869b45c097a7da92e5710531ef4c5731f
Alex Cristea <alexcristea@me.com>
2020-04-15 14:53:10 +0300

    PHX-538 Temp commit

---
+++ src/app/services/AppProvider.js
@@ -0,0 +1,11 @@
+import React, { useState, createContext } from 'react'
+
+export const AppContext = createContext()
+const AppProvider = ({ children }) => {
+    const [active]
+    return (<AppContext.Provider value={{
+
+    }}>{children}</AppContext.Provider>)
+}
+
+export default AppProvider
