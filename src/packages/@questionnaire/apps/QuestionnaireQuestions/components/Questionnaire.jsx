436a2a99ad04abc58d810a47a16aa061330768d3
Alex Cristea <alexcristea@me.com>
2020-04-16 08:22:21 +0300

    PHX-538 Adjust the questionnair buttons

--- src/packages/@questionnaire/apps/QuestionnaireQuestions/components/Questionnaire.jsx
+++ src/packages/@questionnaire/apps/QuestionnaireQuestions/components/Questionnaire.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react'
-import { Button, Space } from '@kogaio'
+import { Button, Space, Flex } from '@kogaio'
+import { navigate } from '@reach/router'
+import { TouchableWithIcon } from '@shared-utils/components'
 
 import { QuestionnaireContext } from 'app/services/QuestionnaireProvider'
 import { Content } from '.'
@@ -7,13 +9,33 @@ import { questionnaireItemsObj } from '../assets'
 
 const Questionnaire = props => {
   const { currentQuestionId, setCurrentQuestionId } = useContext(QuestionnaireContext)
+  const item = questionnaireItemsObj[currentQuestionId]
 
-  const showNextQuestion = () => setCurrentQuestionId(questionnaireItemsObj[currentQuestionId].nextQuestionId)
+  const showNextQuestion = () => setCurrentQuestionId(item.nextQuestionId)
+  const showPrevQuestion = () => setCurrentQuestionId(item.previousQuestionId)
+  const showResults = () => {
+    navigate('/questionnaire/results')
+    setCurrentQuestionId(null)
+  }
 
   return (
-    <Content title={questionnaireItemsObj[currentQuestionId].title} {...props}>
-      <Space mt={6}>
-        <Button onClick={showNextQuestion} title='Next' />
+    <Content title={item.title} {...props}>
+      <Space mt={8}>
+        <Flex justifyContent='space-between'>
+          {item.previousQuestionId ? (
+            <TouchableWithIcon
+              onClick={showPrevQuestion}
+              icon={{ name: 'keyboard_backspace', fontSize: '24px' }}
+              label='Back'
+            />
+          ) : null}
+
+          {item.nextQuestionId ? (
+            <Button onClick={showNextQuestion} title='Next' />
+          ) : (
+            <Button onClick={showResults} title='Submit' />
+          )}
+        </Flex>
       </Space>
     </Content>
   )
