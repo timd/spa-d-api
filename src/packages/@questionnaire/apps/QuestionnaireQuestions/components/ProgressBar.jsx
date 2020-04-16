c0bc8c0d93b5ac8595ed735fb43dca8422e3e72d
Alex Cristea <alexcristea@me.com>
2020-04-16 11:25:33 +0300

    PHX-538 Add progress bar background color

--- src/packages/@questionnaire/apps/QuestionnaireQuestions/components/ProgressBar.jsx
+++ src/packages/@questionnaire/apps/QuestionnaireQuestions/components/ProgressBar.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Box, Flex } from '@kogaio'
+import { themeGet } from '@kogaio/utils'
 
 const ProgressBar = ({ progress, ...props }) => (
-  <Container width={1} height='6px' position='absolute' top='0' left='0' right='0'>
+  <Container>
     <Box bg='brand' width={progress}></Box>
   </Container>
 )
@@ -12,6 +13,7 @@ const ProgressBar = ({ progress, ...props }) => (
 const Container = styled(Flex)`
   width: 100%;
   height: 6px;
+  background: ${themeGet('colors.progress-bg')};
   position: absolute;
   top: 0;
   left: 0;
