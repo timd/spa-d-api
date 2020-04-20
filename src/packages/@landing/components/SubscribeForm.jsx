09dab252582b2ad9388c2383401776c11e8018ca
Sebastian Weikart <sebastian.weikart@finleap.com>
2020-04-20 13:00:41 +0200

    adding mailchimp submit to submit form

--- src/packages/@landing/components/SubscribeForm.jsx
+++ src/packages/@landing/components/SubscribeForm.jsx
@@ -1,84 +1,53 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import { Formik, Form } from 'formik'
+
+import { Formik } from 'formik'
 import { Button, Flex, Space, Typography } from '@kogaio'
 
 import { ValidatedInput } from '@shared-utils/components'
 import { emailFormat } from '@shared-utils/funcs'
 
-const SubscribeForm = ({ isSubscribing, requestSubscribe }) => {
-  const submitForm = async (formValues, actions) => {
-    const { setStatus, setSubmitting } = actions
-    try {
-      const { email } = formValues
-      setStatus(null)
-      const response = await requestSubscribe(email)
-      if (response.success) {
-        //To discuss (we can show a modal with success message)
-      } else if (response.error) {
-        setSubmitting(false)
-        handleCreateError(response.error)
-      }
-    } catch (err) {
-      setSubmitting(false)
-      console.error('* Unexpected error caught while subscribing with email', err)
-    }
-  }
-
-  const handleCreateError = err => {
-    console.error('* Error caught while subscribing with email', err)
-  }
-
-  return (
-    <Formik
-      initialValues={{
-        email: '',
-      }}
-      onSubmit={submitForm}>
-      {({ handleSubmit, isSubmitting, status }) => (
-        <FullWidthForm onSubmit={handleSubmit} noValidate>
-          <Typography color='error' variant='caption'>
-            {status}
-          </Typography>
-
-          <Flex justifyContent='flex-end' flexWrap={{ xs: 'wrap', md: 'nowrap' }}>
-            <ValidatedInput
-              containerStyle={{
-                maxWidth: { xs: '311px', md: 'auto' },
-                width: { xs: 1, md: 'calc(100% - 148px)' },
-                margin: 'auto',
-              }}
-              id='subscribe-email'
-              icLeft='email'
-              name='email'
-              placeholder='Email address'
-              required
-              validate={[emailFormat]}
+const SubscribeForm = () => (
+  <Formik
+    validateOnChange='true'
+    initialValues={{
+      email: '',
+    }}>
+    {({ status, errors, dirty }) => (
+      <form
+        action='https://finleap.us19.list-manage.com/subscribe/post?u=979577850ed7008dcd7b31f0d&amp;id=c79131ac6d'
+        method='POST'
+        target='_blank'
+        noValidate>
+        <Typography color='error' variant='caption'>
+          {status}
+        </Typography>
+        <Flex justifyContent='flex-end' flexWrap={{ xs: 'wrap', md: 'nowrap' }}>
+          <ValidatedInput
+            containerStyle={{
+              maxWidth: { xs: '311px', md: 'auto' },
+              width: { xs: 1, md: 'calc(100% - 148px)' },
+              margin: 'auto',
+            }}
+            id='subscribe-email'
+            icLeft='email'
+            name='EMAIL'
+            placeholder='Email address'
+            required
+            validate={[emailFormat]}
+          />
+          <Space ml={{ xs: 'auto', md: 2 }} mr={{ xs: 'auto', md: 0 }}>
+            <Button
+              disabled={!dirty || (errors.EMAIL && errors.EMAIL.length > 0)}
+              variant='secondary'
+              title='Subscribe'
+              type='submit'
+              width={{ xs: 1, md: 'auto' }}
             />
-            <Space ml={{ xs: 'auto', md: 2 }} mr={{ xs: 'auto', md: 0 }}>
-              <Button
-                disabled={isSubmitting}
-                variant='secondary'
-                title='Subscribe'
-                type='submit'
-                width={{ xs: 1, md: 'auto' }}
-              />
-            </Space>
-          </Flex>
-        </FullWidthForm>
-      )}
-    </Formik>
-  )
-}
-
-const FullWidthForm = styled(Form)`
-  width: 100%;
-`
-
-SubscribeForm.propTypes = {
-  isSubscribing: PropTypes.bool,
-  requestSubscribe: PropTypes.func,
-}
+          </Space>
+        </Flex>
+      </form>
+    )}
+  </Formik>
+)
 
 export default SubscribeForm
