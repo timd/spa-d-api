8fe17c105a07cd962c2023d6eee7ed1dbeb00608
ts.mihail <mihail.salari@thinslices.com>
2020-04-21 14:40:19 +0300

    PHX538 Refactor ProgressBar and Questionnaire

--- src/packages/@questionnaire/apps/QuestionnaireQuestions/components/ProgressBar.jsx
+++ src/packages/@questionnaire/apps/QuestionnaireQuestions/components/ProgressBar.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { Box, Flex } from '@kogaio'
+import { Flex } from '@kogaio'
 import { themeGet } from '@kogaio/utils'
 
 const ProgressBar = ({ progress, ...props }) => (
-  <Container left={{ xs: -24, md: 0 }} right={{ xs: -24, md: 0 }}>
-    <Bar bg='brand' width={progress}></Bar>
-  </Container>
+  <Container left={{ xs: -24, md: 0 }} progress={progress} width={{ xs: '100vw', md: '100%' }} {...props} />
 )
 
 const Container = styled(Flex)`
@@ -15,10 +13,12 @@ const Container = styled(Flex)`
   background: ${themeGet('colors.progress-bg')};
   position: absolute;
   top: 0;
-`
-
-const Bar = styled(Box)`
-  background: ${themeGet('colors.brand')};
+  :after {
+    background: ${themeGet('colors.brand')};
+    content: '';
+    height: 100%;
+    width: ${({ progress }) => progress};
+  }
 `
 
 ProgressBar.propTypes = {
